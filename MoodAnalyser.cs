908755777e6eda7fb8add89d7a0125cd1472b54b
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-10-06 17:35:30 +0530

    [Dhanya R Hegde] : Added RespondHappyOrSadMood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsAndExceptionHandeling
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message) 
+        {
+            this.message = message;
+        }
+        public string analyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                    return "HAPPY";
+            }
+            catch (Exception e)
+            {
+                return "HAPPY";
+            }
+
+
+
+        }
+    }
+}
